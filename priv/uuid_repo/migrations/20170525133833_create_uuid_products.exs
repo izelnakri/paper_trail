e1867b18e401a0a9eca04b8585186efdc05a464f
devvit <baal@baals-iMac.lan>
2018-03-14 21:19:35 +0800

    test with env

--- priv/uuid_repo/migrations/20170525133833_create_uuid_products.exs
+++ priv/uuid_repo/migrations/20170525133833_create_uuid_products.exs
@@ -8,12 +8,5 @@ defmodule PaperTrail.UUIDRepo.Migrations.CreateUuidProducts do
 
       timestamps()
     end
-
-    create table(:items, primary_key: false) do
-      add :item_id, :binary_id, primary_key: true
-      add :title, :string, null: false
-
-      timestamps()
-    end
   end
 end
