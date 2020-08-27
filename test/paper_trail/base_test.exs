2f1643afd5e4e2332a2a91ab9d4105b74b2d2bb7
Izel Nakri <contact@izelnakri.com>
2020-08-27 22:18:36 +0200

    dont pass nil to PaperTrail.insert & update

--- test/paper_trail/base_test.exs
+++ test/paper_trail/base_test.exs
@@ -543,11 +543,11 @@ defmodule PaperTrailTest do
     User.changeset(%User{}, %{token: "fake-token", username: "izelnakri"}) |> @repo.insert!
   end
 
-  defp create_company_with_version(params \\ @create_company_params, options \\ nil) do
+  defp create_company_with_version(params \\ @create_company_params, options \\ []) do
     Company.changeset(%Company{}, params) |> PaperTrail.insert(options)
   end
 
-  defp update_company_with_version(company, params \\ @update_company_params, options \\ nil) do
+  defp update_company_with_version(company, params \\ @update_company_params, options \\ []) do
     Company.changeset(company, params) |> PaperTrail.update(options)
   end
 
