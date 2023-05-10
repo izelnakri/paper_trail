528313b0082c473d44fc91e623c38891b3be248f
Thanya Nitithatsanakul <ammbot@gmail.com>
2023-05-10 19:38:50 +0700

    update test config

--- config/test.exs
+++ config/test.exs
@@ -7,26 +7,26 @@ config :paper_trail, repo: PaperTrail.Repo, originator: [name: :user, model: Use
 
 config :paper_trail, PaperTrail.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: System.get_env("POSTGRES_USER"),
-  password: System.get_env("POSTGRES_PASSWORD"),
+  username: System.get_env("POSTGRES_USER", "postgres"),
+  password: System.get_env("POSTGRES_PASSWORD", "postgres"),
   database: "paper_trail_test",
-  hostname: System.get_env("PG_HOST"),
+  hostname: System.get_env("PG_HOST", "localhost"),
   poolsize: 10
 
 config :paper_trail, PaperTrail.UUIDRepo,
   adapter: Ecto.Adapters.Postgres,
-  username: System.get_env("POSTGRES_USER"),
-  password: System.get_env("POSTGRES_PASSWORD"),
+  username: System.get_env("POSTGRES_USER", "postgres"),
+  password: System.get_env("POSTGRES_PASSWORD", "postgres"),
   database: "paper_trail_uuid_test",
-  hostname: System.get_env("PG_HOST"),
+  hostname: System.get_env("PG_HOST", "localhost"),
   poolsize: 10
 
 config :paper_trail, PaperTrail.UUIDWithCustomNameRepo,
   adapter: Ecto.Adapters.Postgres,
-  username: System.get_env("POSTGRES_USER"),
-  password: System.get_env("POSTGRES_PASSWORD"),
+  username: System.get_env("POSTGRES_USER", "postgres"),
+  password: System.get_env("POSTGRES_PASSWORD", "postgres"),
   database: "paper_trail_uuid_with_custom_name_test",
-  hostname: System.get_env("PG_HOST"),
+  hostname: System.get_env("PG_HOST", "localhost"),
   poolsize: 10
 
 config :logger, level: :warn
