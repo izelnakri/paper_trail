2a809e878f52b9be56be3dd738132a7e995e912b
Richard Kallos <richard@marketops.com>
2024-05-22 09:39:34 -0400

    Remove unnecessary Code.eval_file/1 invocations

--- test/paper_trail/base_test.exs
+++ test/paper_trail/base_test.exs
@@ -30,8 +30,6 @@ defmodule PaperTrailTest do
   setup_all do
     Application.put_env(:paper_trail, :strict_mode, false)
     Application.put_env(:paper_trail, :repo, PaperTrail.Repo)
-    Code.eval_file("lib/paper_trail.ex")
-    Code.eval_file("lib/version.ex")
     :ok
   end
 
